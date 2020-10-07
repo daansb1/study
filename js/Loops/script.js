7324875982812c5cc3adbd5f944374bd3a4607da
Daniel <dbento001@gmail.com>
2020-10-07 11:35:51 -0300

    Update de arquivos, atualizando lista.

---
+++ js/Loops/script.js
@@ -0,0 +1,10 @@
+var x = 0;
+
+document.write("Iniciando o Loop...<br/>")
+
+ while(x = 5) {
+    
+    document.write("numero:"+x+"<br/>")
+
+    x++;
+}
---
+++ js/Refresh/script.js
@@ -0,0 +1,7 @@
+// Comentários...
+/**
+ *  Comentários
+ */
+var nome  = "Daniel";
+
+document.write("<img src='http://www.google.com.br/google.jpg'/>");
